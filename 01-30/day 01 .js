2e97bff08afaa3e19ae64521dd6197a18db7f938
Abdullah Al-Habal <dbnkalhbalb@gmail.com>
2023-05-25 00:23:10 +0300

    initial commit - we just started 25 May 2023

---
+++ 01-30/day 01 .js
@@ -0,0 +1,7 @@
+// 25 May 2023 12:04 AM - Damascus - Syria  
+
+// Hello World 
+console.log("Hello World!")
+
+// in the terminal -- in the same directory 
+// node '.\day 01 .js\'                                     => Hello World!
